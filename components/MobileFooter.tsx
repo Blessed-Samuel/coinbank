e5871d183438389ca5ff2b097a5ec620a7498863
Blessed-Samuel <samuelalisigwe22@gmail.com>
2024-11-19 13:28:14 +0100

    fixed logo display for mobile footer.

--- components/MobileFooter.tsx
+++ components/MobileFooter.tsx
@@ -20,7 +20,7 @@ const MobileFooter: React.FC = () => {
         { name: 'Prices', path: './prices' },
         { name: 'Developers', path: './developers' },
         { name: 'About', path: 'companys/about' },
-        { name: 'Blogs', path: 'companys/blogs' },
+        { name: 'Blog', path: 'companys/blog' },
         { name: 'FAQs', path: 'companys/faqs' },
         { name: 'Support', path: 'companys/support' }
     ];
@@ -30,7 +30,7 @@ const MobileFooter: React.FC = () => {
             <div className="mb-20 rounded-lg shadow-[0_8px_12px_rgba(0,0,0,0.02)] lg:hidden bg-white  text-white py-10">
                 <div className="container mx-auto flex flex-col items-center text-center justify-center space-y-10">
                     <Image
-                        src="./coinBankLogo.svg"
+                        src="../coinBankLogo.svg"
                         alt='coinBankLogo'
                         width={30}
                         height={30}
