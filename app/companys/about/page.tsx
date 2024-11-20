f3af8b1da5b12ae67e8fb8a1bef2a694a37c69b3
Blessed-Samuel <samuelalisigwe22@gmail.com>
2024-11-20 13:47:38 +0100

    Added to about page UI.

--- app/companys/about/page.tsx
+++ app/companys/about/page.tsx
@@ -9,6 +9,8 @@ import GeneralFooter from '@/components/GeneralFooter';
 import Image from 'next/image';
 import Button from '@/components/Button';
 import { FaArrowCircleRight } from 'react-icons/fa';
+import TestimonialCards from '@/components/TestimonialCards';
+import SponsorMarquee from '@/components/Sponsors';
 // import { useRouter } from 'next/navigation';
 
 export const metadata: Metadata = {
@@ -42,7 +44,7 @@ const About = () => {
             </Section>
 
             {/* Mission */}
-            <Section classname='min-h-screen mt-24' customStyles='bg-white'>
+            <Section classname='mt-24' customStyles='bg-white'>
                 <Container>
                     <div className="flex flex-col md:flex-row gap-16 justify-between">
                         {/* Left Content */}
@@ -69,6 +71,133 @@ const About = () => {
                                 <p className="text-mainColorNeutral font-light">Transactions per second</p>
                             </div>
                         </div>
+                    </div>
+                </Container>
+            </Section>
+
+            {/* Quotes */}
+            <Section classname='mt-40' customStyles='bg-white'>
+                <Container>
+                    <div className="flex flex-col gap-12 items-center justify-center">
+                        <Image
+                            src="/quoteImg1.svg"
+                            alt='quoteImg1'
+                            width={30}
+                            height={30}
+                        />
+                        <h1 className="text-mainColorSecondary text-center text-4xl md:text-5xl max-w-4xl font-normal" style={{ lineHeight: '120%' }}>
+                            Its low fees, high rates for staking, and futures markets are all a draw.
+                        </h1>
+                        <div className='flex gap-4 items-center'>
+                            <Image
+                                src="/aboutAvatar.svg"
+                                alt='aboutAvatar'
+                                width={80}
+                                height={80}
+                            />
+                            <div>
+                                <h5 className='text-mainColorSecondary text-xl font-semibold'>John Clayton</h5>
+                                <p className='text-mainColorNeutral font-light'>Investor</p>
+                            </div>
+                        </div>
+                        <Image
+                            src="/quoteImg2.svg"
+                            alt='quoteImg2'
+                            width={30}
+                            height={30}
+                        />
+
+                    </div>
+                </Container>
+            </Section>
+
+            {/* Gallery */}
+            <Section classname='mt-40' customStyles='bg-white'>
+                <Container>
+                    <div className="flex flex-col gap-28 items-center justify-center">
+                        <div className='flex flex-col md:flex-row justify-between gap-16'>
+                            <Image
+                                src="/gallery1.svg"
+                                alt='gallery2-image'
+                                width={100}
+                                height={100}
+                                className='w-auto h-auto'
+                            />
+                            <Image
+                                src="/gallery2.svg"
+                                alt='gallery2-image'
+                                width={100}
+                                height={100}
+                                className='w-auto h-auto'
+                            />
+                        </div>
+                        <div className='flex flex-col md:flex-row gap-12 justify-between'>
+                            <h1 className="md:w-5/12 text-mainColorSecondary text-4xl md:text-5xl max-w-4xl font-normal" style={{ lineHeight: '120%' }}>
+                                The power to chart your own financial course
+                            </h1>
+                            <p className='md:w-5/12 text-mainColorNeutral font-light text-sm'>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet. Amet minim mollit non deserunt ullamco est sit aliqua dolor.</p>
+                        </div>
+                    </div>
+                </Container>
+            </Section>
+
+            {/* Founders Section */}
+            <Section classname="bg-white md:pb-0 pb-0 md:pt-10 pt-20">
+                <Container>
+                    <div className="grid md:grid-cols-3 gap-6 grid-cols-1 mt-20">
+                        <TestimonialCards quote="“Cool crypto currency, fast withdrawals and deposits, way better, than any wallet.”"
+                            image="/My-Pics-2.jpg"
+                            name="Blessed Samuel"
+                            profession="Software Developer"
+                        />
+
+                        <TestimonialCards quote="“It’s worth it, very intuitive and easy to learn about cryptocurrencies.”"
+                            image="/My-Pics-2.jpg"
+                            name="Blessed Samuel"
+                            profession="Founder"
+                        />
+
+                        <TestimonialCards quote="“Best customer service so far. Amazing in all areas best centralized exchange.”"
+                            image="/My-Pics-2.jpg"
+                            name="Blessed Samuel"
+                            profession="Investors"
+                        />
+                    </div>
+                </Container>
+            </Section>
+
+            <Section bgClass='aboutsectionH2Bg' classname='mt-40 md:pb-0 pb-0 md:pt-10 pt-20' customStyles='bg-white'>
+                <Container>
+                    <div className="space-y-12">
+                        <div className='flex gap-4 items-center flex-col'>
+                            <h6 className="text-mainColorPrimary font-light">PARTNERS</h6>
+                            <h1 className="text-mainColorSecondary text-4xl md:text-5xl leading-[120%] text-center font-bold">We're backed by the best</h1>
+                            <p className="text-mainColorNeutral font-light">Trusted by these blockchains leading industries </p>
+                        </div>
+
+                        <div id="marqueeEl">
+                            <SponsorMarquee direction="left" />
+                            <SponsorMarquee direction="right" />
+                        </div>
+                    </div>
+                </Container>
+            </Section>
+
+            {/* Call to Action 2 Section */}
+            <Section classname="bg-[#F7F6FE] md:pb-10 pb-0 md:pt-20 pt-40">
+                <Container>
+                    <div className='w-auto h-auto p-20 rounded-3xl flex flex-col gap-12 items-center' style={{
+                        backgroundImage: "url(/CTAbg.svg)",
+                        backgroundSize: 'cover',
+                        backgroundPosition: 'center'
+                    }}>
+                        <div className='flex flex-col items-center gap-6'>
+                            <h1 className="text-white text-center text-4xl md:text-5xl max-w-2xl font-normal" style={{ lineHeight: '120%' }}>
+                                Join our mission
+                            </h1>
+                            <p className="max-w-xl text-white text-center font-extralight">Join our mission to bring greater choice, independence, and opportunity to individuals around the world.</p>
+                        </div>
+                        <Button className="px-4 py-2 text-nowrap bg-mainColorPrimary">Visit Careers Page</Button>
                     </div>
                 </Container>
             </Section>
