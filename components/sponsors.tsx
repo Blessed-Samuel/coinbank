5f4cb0ad460e00d7806473ab7836c727d626bc54
Blessed-Samuel <samuelalisigwe22@gmail.com>
2024-11-24 15:25:06 +0100

    Fixed Brand slider in sponsors page.

--- components/sponsors.tsx
+++ components/sponsors.tsx
@@ -1,23 +1,23 @@
-import React from 'react';
-import Image from 'next/image';
+import React from "react";
+import Image from "next/image";
 
 type SponsorMarqueeProps = {
-    direction?: 'left' | 'right';
+    direction?: "left" | "right";
 };
 
-const SponsorMarquee: React.FC<SponsorMarqueeProps> = ({ direction = 'left' }) => {
+const SponsorMarquee: React.FC<SponsorMarqueeProps> = ({ direction = "left" }) => {
     // Determine which animation to apply based on the direction prop
-    const animationClass = direction === 'left' ? 'animate-marquee' : 'animate-marqueeReverse';
+    const animationClass = direction === "left" ? "animate-marquee" : "animate-marqueeReverse";
 
     return (
         <div className="overflow-hidden w-full">
             <div className={`flex ${animationClass} space-x-2`}>
-                <Image src="/sponsor1.svg" alt="Sponsor 1" width={100} height={100} className="w-auto h-auto" />
+                <Image src="/sponsor1.svg" alt="Sponsor1" width={100} height={100} className="w-auto h-auto" />
+                <Image src="/sponsor2.svg" alt="Sponsor2" width={100} height={100} className="w-auto h-auto" />
+                <Image src="/sponsor3.svg" alt="Sponsor3" width={100} height={100} className="w-auto h-auto" />
+                <Image src="/sponsor4.svg" alt="Sponsor4" width={100} height={100} className="w-auto h-auto" />
                 <Image src="/sponsor2.svg" alt="Sponsor 2" width={100} height={100} className="w-auto h-auto" />
-                <Image src="/sponsor3.svg" alt="Sponsor 3" width={100} height={100} className="w-auto h-auto" />
-                <Image src="/sponsor4.svg" alt="Sponsor 4" width={100} height={100} className="w-auto h-auto" />
-                <Image src="/sponsor2.svg" alt="Sponsor 2" width={100} height={100} className="w-auto h-auto" />
-                <Image src="/sponsor1.svg" alt="Sponsor 1" width={100} height={100} className="w-auto h-auto" />
+                <Image src="/sponsor1.svg" alt="Sponsor1" width={100} height={100} className="w-auto h-auto" />
             </div>
         </div>
     );
